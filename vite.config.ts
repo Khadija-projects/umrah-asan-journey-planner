3bb5933db3722492964a81b14d33c112a85cac0c
Khadija-projects <kiju.sohail@gmail.com>
2025-07-18 06:26:48 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import type { UserConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
   base: '/umrah-asan-journey-planner/',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
-  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        entryFileNames: 'assets/[name]-[hash].js'
+        entryFileNames: '[name]-[hash].js',
+        assetFileNames: '[name]-[hash][extname]'
       }
     }
   }
-} as UserConfig)
+})
